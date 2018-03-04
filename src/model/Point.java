d64e3a69031509f0dd1153f1e75945cce1e247bf
Semenov.Matvey <matek3022@gmail.com>
2018-03-04 19:58:22 +0400

    Добавлена библиотека анотаций, добавлен класс точка

---
+++ src/model/Point.java
@@ -0,0 +1,48 @@
+package model;
+
+import javafx.util.Pair;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.LinkedList;
+
+public class Point {
+    /**
+     * характеризует тип ячейки
+     * 0 - белая
+     * 1 - черная
+     * вероятны другие значения в будущем...
+     */
+    private byte value;
+    /**
+     * координаты в матрице
+     */
+    private Pair<Integer, Integer> coord;
+
+    /**
+     * точки в общем кластере
+     */
+    private LinkedList<Point> clusterFriends;
+
+    public Point(byte value, @Nonnull Pair<Integer, Integer> coord) {
+        this.value = value;
+        this.coord = coord;
+    }
+
+    public Point(byte value, int coordX, int coordY) {
+        this.value = value;
+        this.coord = new Pair<>(coordX, coordY);
+    }
+
+    public void addClasterFriend(@Nonnull Point friend) {
+        if (clusterFriends == null) {
+            clusterFriends = new LinkedList<>();
+        }
+        clusterFriends.add(friend);
+    }
+
+    @Nullable
+    public LinkedList<Point> getClusterFriends() {
+        return clusterFriends;
+    }
+}
