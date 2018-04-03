b56640820dfab984968475a59f9488797167f349
Matvey.Semenov <matek3022@gmail.com>
2018-04-03 09:38:26 +0400

    Запись в excel файл и разбиение по потокам

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import model.Point;
 import model.Table;
-import utils.Tests;
+import utils.ExcelUtils;
 
 import java.util.LinkedList;
 
 
 public class Main {
+    public static final int MAX_THREADS = 4;
+    public static final int MAX_ITERATION = 20;
+
+    public static int currThreads = 0;
+    public static int currIter = 0;
 
     public static void main(String[] argv) {
 //        startNewThread(75, 75, 0.1d);
@@ -29,10 +34,23 @@ public class Main {
 //        startNewThread(100, 100, 0.9d);
 //        startNewThread(150, 150, 0.4d);
 //        startNewThread(20, 20, 0.1d);
-        startNewThread(Tests.getChessMatrix50x50());
+//        startNewThread(Tests.getChessMatrix50x50());
 //        startNewThread(Tests.getGradMatrix50x50(true));
 //        startNewThread(Tests.getGradMatrix50x50(false));
 //        startNewThread(Tests.getRainMatrix50x50());
+//        new MainForm();
+        while (currIter != MAX_ITERATION) {
+            if (currThreads < MAX_THREADS) {
+                currIter++;
+                startNewThread(50, 50, 0.4d, currIter);
+            } else {
+                try {
+                    Thread.sleep(500L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static void startNewThread(int m, int n, double p) {
@@ -41,13 +59,29 @@ public class Main {
             public void run() {
                 long time = System.currentTimeMillis() / 1000L;
                 Table table = new Table(m, n, p);
+                time = System.currentTimeMillis() / 1000L - time;
                 System.out.println();
                 System.out.println("Cluster count: " + table.getClusterCount());
                 System.out.println("Table minLength: " + table.getMinLength());
                 System.out.println("Table road width: " + table.getRoadWidth());
-                System.out.println("Time: " + (System.currentTimeMillis() / 1000L - time));
+                System.out.println("Time: " + time);
                 System.out.println(String.format("Params: m = %s, n = %s, p = %s", m, n, p));
                 table.printTable(false);
+            }
+        }).start();
+    }
+
+    public static void startNewThread(int m, int n, double p, int currIter) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                currThreads++;
+                long time = System.currentTimeMillis() / 1000L;
+                Table table = new Table(m, n, p);
+                time = System.currentTimeMillis() / 1000L - time;
+                System.out.println("Текущая выполненная операция = " + currIter);
+                ExcelUtils.writeTableToFile("test.xls", table, currIter, time);
+                currThreads--;
             }
         }).start();
     }
---
+++ src/utils/ExcelUtils.java
@@ -0,0 +1,66 @@
+package utils;
+
+import model.Table;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public abstract class ExcelUtils {
+
+    public static synchronized void writeTableToFile(String fileName, Table table, int currIter, long time) {
+        HSSFWorkbook book;
+        try {
+            book = new HSSFWorkbook(new FileInputStream(fileName));
+        } catch (IOException e) {
+            book = new HSSFWorkbook();
+        }
+        String sheetName = "Table info";
+        Sheet sheet = book.getSheet(sheetName);
+        if (sheet == null) sheet = book.createSheet(sheetName);
+
+        // Нумерация начинается с нуля
+        Row row = sheet.getRow(0);
+        if (row == null) row = sheet.createRow(0);
+
+        row.createCell(0).setCellValue("Длина пути");
+        row.createCell(1).setCellValue("Ширина пути");
+        row.createCell(2).setCellValue("Количество кластеров всего");
+        row.createCell(3).setCellValue("Время");
+
+        row.createCell(5).setCellValue("Ширина = " + table.getN());
+        row.createCell(6).setCellValue("Высота = " + table.getM());
+        row.createCell(7).setCellValue("Вероятность = " + table.getP());
+
+        // Меняем размер столбца
+        sheet.autoSizeColumn(0);
+        sheet.autoSizeColumn(1);
+        sheet.autoSizeColumn(2);
+        sheet.autoSizeColumn(3);
+
+        sheet.autoSizeColumn(5);
+        sheet.autoSizeColumn(6);
+        sheet.autoSizeColumn(7);
+
+        Row tableRow = sheet.getRow(currIter);
+        if (tableRow == null) tableRow = sheet.createRow(currIter);
+        tableRow.createCell(0).setCellValue(table.getMinLength());
+        tableRow.createCell(1).setCellValue(table.getRoadWidth());
+        tableRow.createCell(2).setCellValue(table.getClusterCount());
+        tableRow.createCell(3).setCellValue(time);
+
+        // Записываем всё в файл
+        try {
+            book.write(new FileOutputStream(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public static synchronized Table readFromFile(String fileName) {
+//
+//    }
+}
